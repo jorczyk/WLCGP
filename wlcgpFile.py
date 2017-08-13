677a4c35382ed16e831551cb06a87e634edd156a
Piotr Majorczyk <smajorc@gmail.comgit config --global user.email smajorc@gmail.comgit config --global user.email smajorc@gmail.com>
2017-08-13 23:58:31 +0200

    WLCGP is working but wrong(?) values are obtained

--- wlcgpFile.py
+++ wlcgpFile.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+#TODO -- zle obliczanie wartosci w histogramie i te dziwne 0 kolumny
 
 def wlcgp(image):
     dImage = np.double(image)
-    ysize, xsize = dImage.shape #ysize, xsize = dImage.shape
+    ysize, xsize = dImage.shape  # ysize, xsize = dImage.shape
 
-    #print xsize
+    # print xsize
     bSizey = 3
     bSizex = 3
 
@@ -22,27 +23,27 @@ def wlcgp(image):
     dx = xsize - bSizex
     dy = ysize - bSizey
 
-    #print dx, dy
+    # print dx, dy
     # init the result matrix with 0
 
     dDifferentialExcitation = np.zeros((dy + 2, dx + 2))
     dGradientOrientation = np.zeros((dy + 2, dx + 2))
 
-    print np.shape(dDifferentialExcitation) #ok
+    # print np.shape(dDifferentialExcitation) #ok
     # compute WLCGP code per pixel
     b = []
-    count = 0
-    y = 1 #2
+    # count = 0
+    y = 1  # 2
     while y <= ysize - 2:
-        #y += 1
-        count += 1
-        #print y
-        x = 1 #2
-        while x <= xsize - 2: #x <= xsize - 1:
-            #x += 1
+        # y += 1
+        # count += 1
+        # print y
+        x = 1  # 2
+        while x <= xsize - 2:  # x <= xsize - 1:
+            # x += 1
             N = dImage[y - 1:y + 1, x - 1:x + 1]
-            #print xsize
-            #print x, y
+            # print xsize
+            # print x, y
             center = dImage[y, x]
 
             v00 = (abs(dImage[y + 1, x] - dImage[y + 1, x - 1])) + abs((dImage[y + 1, x + 1] - dImage[y + 1, x])) + abs(
@@ -64,10 +65,10 @@ def wlcgp(image):
                       dImage[y - 1, x + 1] \
                       + dImage[y, x + 1] + dImage[y + 1, x + 1]) / 9
 
-            b = np.append(b,v00)
+            # b = np.append(b,v00)
 
             if (v01 != 0):
-                dDifferentialExcitation[y, x] = np.math.atan(ALPHA * v00 / v01) #dlaczego
+                dDifferentialExcitation[y, x] = np.math.atan(ALPHA * v00 / v01)
             else:
                 dDifferentialExcitation[y, x] = 0.1
 
@@ -96,7 +97,7 @@ def wlcgp(image):
                     # end
             x += 1
         y += 1
-    print b
+    # print b
     # end
 
     # histogram
@@ -110,37 +111,53 @@ def wlcgp(image):
     # cVal = #Cval = -pi/2:pi/C:pi/2; % Excitation
 
     # cValcen = #(Cval(1:end-1)+Cval(2:end)) #
-    cValcen = np.arange(-PI / 2 + PI / C, PI / 2 + (2 * PI / C),
+    cValcen = np.arange(-(PI / 2), PI / 2 + (PI / C) - 0.0000001,
                         PI / C)  # If bins is a sequence, it defines the bin edges, including the rightmost edge,
     # allowing for non-uniform bin widths.
 
-    print np.size(cValcen)
+    # cValcen = np.arange(-(PI / 2 + PI / C), PI / 2 + (2 * PI / C),
+    #                     PI / C) ////+ (1/2*PI / C)
+
+    print "cvalen: " + str(np.size(cValcen))
     tVal = np.arange(0, 360 + 360 / T, 360 / T)
     # Tval = 0:360/T:360;
 
     h2d = np.zeros((C, T))
-    print np.shape(h2d)
+    print "h2d size: " + str(np.shape(h2d))
 
-    i = 1
-    while i <= T:
-        i += 1
+    i = 0
+    temp = []
+    # while i <= T-1:
+    for i in range(T):  # T
         if i > 1:
-            temp = dDifferentialExcitation[(dGradientOrientation > tVal[i]) & (dGradientOrientation <= tVal[i + 1])] #boolean?
+            temp = dDifferentialExcitation[
+                (dGradientOrientation > tVal[i]) & (dGradientOrientation <= tVal[i + 1])]  # boolean?
         else:
-            temp = dDifferentialExcitation[(dGradientOrientation >= tVal[i]) & (dGradientOrientation <= tVal[i + 1])] #chyba cos tu nie gra
+            temp = dDifferentialExcitation[
+                (dGradientOrientation >= tVal[i]) & (dGradientOrientation <= tVal[i + 1])]  # chyba cos tu nie gra
         # end
-        print temp
-        h2d[:, i] = np.histogram(temp, cValcen) #!!!!!!!!!
+
+        # print dDifferentialExcitation #ok
+        # print "temp: " + str(temp)
+        print "histogram: " + str((np.histogram(temp, cValcen)[0]))
+        h2d[:, i] = np.histogram(temp, cValcen)[0]
+        # zle wartosci - ale jak beda dla kazdego takie same to luz ALE SA 0!! --do poprawienia!!!
     # end
+
+    print np.size(np.histogram(temp, cValcen)[0])
 
     h = np.transpose(h2d)
     h = np.reshape(h, (T, S, M))
 
-    j = 1
-    while j <= M:
-        j += 1
+    j = 0
+    temph = np.empty(0)
+    for j in range(M):
+    # while j <= M:
+        # j += 1
         temp = h[:, :, j]
-        temph = np.concatenate[temph, temp[:]]  # h = reshape(h,[T,S,M]);
+        # temph = np.concatenate(temph, temp[:])  # h = reshape(h,[T,S,M]);
+        temph = np.append(temph, temp[:])
     # end
     h1d = temph
+    print np.size(h1d) #ok
     return h1d
