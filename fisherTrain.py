c133dccf31d028e6a7321d6c5aa25af501b017d1
Piotr Majorczyk <smajorc@gmail.comgit config --global user.email smajorc@gmail.comgit config --global user.email smajorc@gmail.com>
2017-08-24 03:25:49 +0200

    fixing performance

--- fisherTrain.py
+++ fisherTrain.py
@@ -10,7 +10,9 @@ import numpy as np
 def fisher(X, c, ni):
     r, n = X.shape
     XT = np.transpose(X)
-    mxTot = np.mean(XT)  # mx_tot = mean(XT)';
+    mxTot = np.mean(XT, 0)  # OK
+
+    # print mxTot.shape #OK
 
     # MXi = []
     temp = []
@@ -30,19 +32,14 @@ def fisher(X, c, ni):
         # print "i: " + str(c)
         # print X.shape
         # j = 0
-        for j in range(1, ni, 1):  # ni + 1
-            # print (ni * (i - 1)) + j -1 #ok
-            # print (j * i)
-            # print ((ni * (i - 1)) + (j * i))
+        # if i == 1:
+        for j in range(1, ni + 1, 1):  # ni + 1
             # Xi[:, i] = X[:, ni * (i - 1) + range(1, ni)[j] * i]  # !!!!!!!!!! 1:ni range(1, ni)
-            # print X[:, (ni * (i - 1)) + j - 1]
-            Xi[:, i] = X[:, (ni * (i - 1)) + j - 1]  # OK?
-            # ni*(i-1)+1:ni*i
-        # print Xi #ok?
-        # print "loop"
+            # Xi[:, i-1] = X[:, (ni * (i - 1)) + j - 1]  # OK?
+            Xi[:, j - 1] = X[:, (i - 1) * ni + j - 1]  # OK?
+
         XiT = np.transpose(Xi)
-        # # print np.transpose(np.mean(XiT,1))
-        # print XiT.shape
+
         temp = np.transpose(np.mean(XiT, 0))
         # print np.mean(XiT,0)
 
@@ -53,15 +50,23 @@ def fisher(X, c, ni):
         MXi[:, i - 1] = temp
 
         # end
-    # print MXi.shape
+    # print MXi #ok
+
+    # print X #ok
+    # print Xi #ok
+
     ##################
+    # print MXi.shape #ok
+    # print mxTot.shape
+
+    mxTot = mxTot.reshape((mxTot.shape[0], 1))
 
     FXB = MXi - mxTot * np.ones((1, c))
     SB = ni * FXB.dot(np.transpose(FXB))  # SB = ni * FXB * np.transpose(FXB) sprawdzic czy dobry wynik; rozmiar OK
 
-    # print FXB.shape
-    # print SB.shape
-    # i = 1
+    # print FXB #ok
+    # print SB #ok
+    i = 1
     MXi = np.asmatrix(MXi)
     # print X.shape
     M = np.zeros(X.shape)
@@ -75,7 +80,11 @@ def fisher(X, c, ni):
             M[:, (ni * (i - 1)) + j - 1] = np.asarray(MXi[:, i - 1] * np.ones((1, ni)))[:,
                                            j - 1]  # !!!!!MXi[:, i] * np.ones((1, ni)) ????????????????????????
     # end
+    # print M #ok
+
     # print M.shape
+    #
+    #
 
 
     SW = (X - M).dot(np.transpose(X - M))  # (X - M) * np.transpose(X - M)
@@ -85,34 +94,36 @@ def fisher(X, c, ni):
     # print ST.shape #OK
     U, S, V = np.linalg.svd(SW)
     S = np.diag(S)
-    # print U.shape #(7x7) OK
-    # print S.shape #OK
-    # print V.shape #(7x7) OK
 
-    # rk = np.rank(SW)  # cos dzialalo zle
+    # print SW #ok
+    # print ST #ok
+
+    # # rk = np.rank(SW)  # cos dzialalo zle
     rk = np.linalg.matrix_rank(SW)
-    # print rk
-    ##################################
+    # print SW.shape
+    # print rk #ok
 
-    # Q = U
-    # Q[:, 1:rk] = []  # Q(:,1:rk)=[];
-    Q = np.zeros((rk, c))  # c-1???
+    if rk == U.shape[1]:
+        Q = np.matrix((None))
+    else:
+        Q = np.zeros((U.shape[0],U.shape[1]-rk))
+        for i in range(U.shape[1]-rk):
+            # print U.shape[1]-i
+            Q[:, i] = U[:, rk + i]
 
-    # print c
-    # print
-    for i in range(c):
-        # print U.shape[1]-i
-        Q[:, i] = U[:, U.shape[1] + i - c]
+    # print U #ok
+    # print Q #ok
 
-    # print U #ok --chyba
-    # print Q #ok --chyba
+    # print Q.shape
+    # print SB.shape
 
-    ##################################
-    SBnew = Q.dot(np.transpose(Q)) * SB * np.transpose(Q.dot(np.transpose(Q)))  # romiar OK
+    SBnew = Q.dot(np.transpose(Q)).dot(SB).dot(np.transpose(Q.dot(np.transpose(Q))))  # OK
 
-    # print SBnew.shape
+    # print SBnew
     E1, EV1, E2 = np.linalg.svd(SBnew)
     EV1 = np.diag(EV1)
+
+##########################################################################################
 
     # sig2 = np.isreal(E1)
     # print np.all(np.isreal((E1)))
@@ -120,30 +131,30 @@ def fisher(X, c, ni):
         E1 = np.real(E1)
         EV1 = np.real(EV1)
     # end
+    #
+    # EV = np.sum(EV1, 0).reshape((1, EV1.shape[1]))
+    # # print EV.shape
+    # rk1 = np.linalg.matrix_rank(EV1)  # powinno byc 2 jest 11
+    # # rk1=2 #FOR TESTING ONLY
+    # # rk1 = np.rank(EV1)
+    # # print EV1
+    #
+    # E = np.zeros((E1.shape[0], rk1))
+    # # i = 1
+    # # while i <= rk1:
+    # #     i += 1
+    #
+    # # print E.shape
+    # # print E1.shape
+    # for i in range(rk1):
+    #     maxValue = np.amax(EV)  # [max_value,coln]=max(EV);
+    #     coln = np.argmax(EV)
+    #     # print coln
+    #     E[:, i] = E1[:, coln]
+    #     EV[:, coln] = 0
+    # # end
+    # P = np.transpose(E[:, :]).dot(X)  # np.transpose(E[:, :]) * X
+    # result = (P, E)
+    # # result = fisherResult(P, E)
 
-    EV = np.sum(EV1, 0).reshape((1, EV1.shape[1]))
-    # print EV.shape
-    rk1 = np.linalg.matrix_rank(EV1)  # powinno byc 2 jest 11
-    # rk1=2 #FOR TESTING ONLY
-    # rk1 = np.rank(EV1)
-    # print EV1
-
-    E = np.zeros((E1.shape[0], rk1))
-    # i = 1
-    # while i <= rk1:
-    #     i += 1
-
-    # print E.shape
-    # print E1.shape
-    for i in range(rk1):
-        maxValue = np.amax(EV)  # [max_value,coln]=max(EV);
-        coln = np.argmax(EV)
-        # print coln
-        E[:, i] = E1[:, coln]
-        EV[:, coln] = 0
-    # end
-    P = np.transpose(E[:, :]).dot(X)  # np.transpose(E[:, :]) * X
-    result = (P, E)
-    # result = fisherResult(P, E)
-
-    return result
+    return 1  # result
--- test/testing-test.py
+++ test/testing-test.py
@@ -54,8 +54,10 @@ for i in range(1, NumPerson + 1):
 
 allsamples = allsamples.reshape((NumPerClassTrain * NumPerson, lbpI.size))  # size ok
 # print allsamples.shape
-sampleMean = np.mean(allsamples,0).reshape((1,allsamples.shape[1]))
+sampleMean = np.mean(allsamples, 0).reshape((1, allsamples.shape[1]))
 nTrain = np.size(allsamples, 0)
+
+# print allsamples.shape
 
 # print sampleMean.shape
 
@@ -64,12 +66,22 @@ for i in range(nTrain):
     xmean[i, :] = allsamples[i, :] - sampleMean
 # end
 
-# print xmean[:,700] #rozmiar i wyniki ok
+# print xmean.shape
+# print xmean[:,5726] #rozmiar i wyniki ok -- na pewno OK
+
+# np.savetxt("foo.csv", xmean, delimiter=";")
 
 ###################################################
 # PCA
+
+# print xmean[:,1872] #OK
+# print np.transpose(xmean)[1831,:] #OK
+# print xmean.shape
+
 sigma = xmean.dot(np.transpose(xmean))
 
+# print sigma/10000 #OK
+# print (sigma/10000)[4,:]
 # print sigma
 
 d, v = np.linalg.eig(sigma)
@@ -80,24 +92,54 @@ d, v = np.linalg.eig(sigma)
 # dTemp = np.zeros((d.size,d.size))
 
 d1 = np.squeeze(np.asarray(d))  # rozmiar chyba ok
-d2 = d1.sort
+
+# print d1.shape #6,5,1,4,3,2 wartosci ok
+
+# d2 = d1.sort
+d2 = np.sort(d1)
+# print d1 #ok
+# print d2 #ok
 index = np.argsort(d1)
+# print index #ok
+v2 = np.zeros(v.shape)
 
-# print d1 #3,4,2,1
-# print v #-4,-3,-1,2 --eigeny moga sie nie zgadzac bo tak
-# print d #4,3,1,2
+# for indx in np.nditer(index):
+for i in range(index.size): # po tym macierze sa identyczne jak te z matlaba
+    v2[:, i] = v[:,index[i]]
 
-rows, cols = v.shape
+v2[:,v2.shape[1]-1] = v2[:,v2.shape[1]-1] * -1
+index = range(0,len(index))
+
+# print v2
+
+# print v #wyniki OK - zawsze jedna kolumna z "-"
+
+# print d1  # 3,4,2,1
+# print v #-4,-3,-1,2 --eigeny moga sie nie zgadzac bo tak kolejnosc musi sie zgadzac z z v[:,i] musi odpowiadac w[i]
+# print d #4,3,1,2 --znaki nie maja znaczenia dla eigenvectorow, wiec formalnie wszystko jest ok
+
+rows, cols = v.shape  # ok
 
 vsort = np.zeros((rows, cols))
-dsort = [0] * index.size
+# dsort = [0] * index.size
+dsort = [0] * len(index)
+# print v2 #ok
+# print index
+# print d2
+# print cols
+
 
 for i in range(cols):
-    vsort[:, i] = v[:, index[cols - i - 1]]  # !!
-    dsort[i] = d1[index[cols - i - 1]]
+    vsort[:, i] = v2[:, index[cols - i - 1]]  # kolejnosc nie ma znaczenia bo sobie odpowiadaja
+    dsort[i] = d2[index[
+        cols - i - 1]]  # czy tu ni epowinno byc d2????? - w orignale jest d1 ale maja te d1 zawsze posortowane i
+    # jest to tak jakby d2 tyle ze przy d2 wywala blad w fisherTrain Q[:, i] = U[:, U.shape[1] + i - c] tylko w sumie dlaczego?
 
-# print dsort #elementy te same, zmieniona kolejnosc
+# print dsort
+# print dsort #elementy te same, zmieniona kolejnosc ale to nie powinno nic zmienic
 # print vsort #elementy z -1 i zmieniona kolejnosc
+# print np.size(dsort)
+# print dsort wartosci nie do konca te same
 
 
 dsum = np.sum(dsort)  # dsum = sum(dsort);
@@ -108,11 +150,10 @@ dsum = np.sum(dsort)  # dsum = sum(dsort);
 dsumExtract = 0
 p = 0
 
-while dsumExtract / dsum < 0.95:
-    dsumExtract = np.sum(dsort[0:p-1])  # (dsort[1:p])
+while dsumExtract / dsum < 0.95: #PO CO???
+    dsumExtract = np.sum(dsort[0:p - 1])  # (dsort[1:p])
     p = p + 1
 
-###############
 
 # print dsumExtract #ok
 
@@ -123,103 +164,118 @@ p = nTrain - 1  # nTrain - 1
 
 # print ((vsort[:, i]).shape) #ok
 # print (dsort[1]** (-1 / 2)) #ok
-# print dsort
+
+# print p #ok
+# print dsort #ok
+# print vsort #ok
 
 base = np.zeros((allsamples.shape[1], p))
+
+# print base.shape #ok
+
 while (i < p) & (dsort[i] > 0):  # (i <= p)!
-    base[:, i] = (dsort[i] ** (-1 / 2)) * (np.transpose(xmean).dot(
-        vsort[:, i]))  # dsort[i] ** (-1 / 2) * np.transpose(xmean) * vsort[:, i]
+    base[:, i] = ((1 / np.math.sqrt(dsort[i])) * np.transpose(xmean).dot(
+        vsort[:, i]))  # dsort[i] ** (-1 / 2) * np.transpose(xmean) * vsort[:, i] SPRAWDZIC KOLEJNOSC DZIALAN
+    # print i#ok
+    # print dsort[i]#ok
+    # print vsort[:,i] #ok
+    # print (1 / np.math.sqrt(dsort[i])) #ok
     i = i + 1
-# print base[750,0] #wartosci inne przez inne eigeny!
-    # print base.shape
-    # print p
+# print np.transpose(xmean)[6703,:]#ok
+
+# print base[750,0] #OK
+# print base.shape #ok
+# print p
+
+
+##########################################
 
 allcoor = allsamples.dot(base)
 #
-# print allcoor
+# print allcoor#ok
 # print allcoor.shape #ok numperson jest wazne
 
 temp = fisherTrain.fisher(np.transpose(allcoor), NumPerson, NumPerClassTrain)  # !!!
 
-P = temp[0]
-E = temp[1]
-accu = 0
-
+# P = temp[0]
+# E = temp[1]
+# accu = 0
+#
 # print E.shape
 # print P.shape
-
-####################################################
-
-# Testing
-
-div = 0
-
-for i in range(1, NumPerson + 1):  # 1
-    for j in range(NumPerClassTrain + 1, NumPerClass + 1):  # 1(1, NumPerClassTest + 1):
-        pathname = filepath + "\\s" + str(i) + "\\" + str(j) + ".pgm"
-
-        img2 = cv2.imread(pathname, 0)
-        img2 = ((img2 - np.mean(img2)) + 128) / np.std(img2) * 20
-        iiCell = commons.block(numx, numy, img2)
-        lbpII = []
-        lbpII = np.asarray(lbpII)
-
-        for k in range(numx):
-            for m in range(numy):
-                iiCellBlock = iiCell[k, m]  # k,m
-                blockLBPII = wlcgpFile.wlcgp(iiCellBlock)
-                blockLBPII = np.transpose(blockLBPII)
-                if (m == 0) & (k == 0):
-                    lbpII = blockLBPII
-                else:
-                    lbpII = np.concatenate((lbpII, blockLBPII))  # LBP_I=[LBP_I,Block_LBP_I]; moze byc concatenate
-                    # end
-        # end
-        lbpII = lbpII.reshape(1, lbpII.shape[0])
-        tcoor = lbpII.dot(base)  # lbpII * base// base trzeba tu liczyc!!! //rozmiar ok
-
-        # print np.transpose(E).shape #ok
-        # print np.transpose(tcoor).shape #ok
-
-        tcoor = np.transpose(E).dot(np.transpose(tcoor))  # E trzeba bedzie tu liczyc? ale z czego?
-
-        # print tcoor.shape #ok
-        # k = 1
-        # mdist = []
-        # while k <= nTrain:  # co z tym nTrain???
-        #     k += 1
-        mdist = [None] * nTrain
-        for k in range(nTrain):
-            mdist[k] = np.linalg.norm(tcoor - P[:, k]) #P trzeba wyciagac
-        # end
-
-        ####################################
-
-        # 3 NN algorithm
-        # dist, index2 = np.sort(mdist)
-        index2 = np.argsort(mdist)
-        dist = mdist.sort
-
-        class1 = int(np.math.floor(index2[1] / NumPerClassTrain - 0.1) + 1)  # NumPerClassTrain - skad to ma byc potem brane?
-        class2 = int(np.math.floor(index2[2] / NumPerClassTrain - 0.1) + 1)
-        class3 = int(np.math.floor(index2[3] / NumPerClassTrain - 0.1) + 1)
-
-        if (class1 != class2) & (class2 != class3):
-            result = class1
-        else:
-            if class1 == class2:
-                result = class1
-            if class2 == class3:
-                result = class2
-        # end
-
-        div = div + 1
-
-        # print "Result: " + str(result) + " Actual: " + str(i)
-        if result == i:
-            accu = accu + 1
-# print accu
-# print div
-accuracyRate = float(accu)/float(div)
-
+#
+# ###################################################
+#
+# # Testing
+#
+# div = 0
+#
+# for i in range(1, NumPerson + 1):  # 1
+#     for j in range(NumPerClassTrain + 1, NumPerClass + 1):  # 1(1, NumPerClassTest + 1):
+#         pathname = filepath + "\\s" + str(i) + "\\" + str(j) + ".pgm"
+#
+#         img2 = cv2.imread(pathname, 0)
+#         img2 = ((img2 - np.mean(img2)) + 128) / np.std(img2) * 20
+#         iiCell = commons.block(numx, numy, img2)
+#         lbpII = []
+#         lbpII = np.asarray(lbpII)
+#
+#         for k in range(numx):
+#             for m in range(numy):
+#                 iiCellBlock = iiCell[k, m]  # k,m
+#                 blockLBPII = wlcgpFile.wlcgp(iiCellBlock)
+#                 blockLBPII = np.transpose(blockLBPII)
+#                 if (m == 0) & (k == 0):
+#                     lbpII = blockLBPII
+#                 else:
+#                     lbpII = np.concatenate((lbpII, blockLBPII))  # LBP_I=[LBP_I,Block_LBP_I]; moze byc concatenate
+#                     # end
+#         # end
+#         lbpII = lbpII.reshape(1, lbpII.shape[0])
+#         tcoor = lbpII.dot(base)  # lbpII * base// base trzeba tu liczyc!!! //rozmiar ok
+#
+#         # print np.transpose(E).shape #ok
+#         # print np.transpose(tcoor).shape #ok
+#
+#         tcoor = np.transpose(E).dot(np.transpose(tcoor))  # E trzeba bedzie tu liczyc? ale z czego?
+#
+#         # print tcoor.shape #ok
+#         # k = 1
+#         # mdist = []
+#         # while k <= nTrain:  # co z tym nTrain???
+#         #     k += 1
+#         mdist = [None] * nTrain
+#         for k in range(nTrain):
+#             mdist[k] = np.linalg.norm(tcoor - P[:, k]) #P trzeba wyciagac
+#         # end
+#
+#         ####################################
+#
+#         # 3 NN algorithm
+#         # dist, index2 = np.sort(mdist)
+#         index2 = np.argsort(mdist)
+#         dist = mdist.sort
+#
+#         class1 = int(np.math.floor(index2[1] / NumPerClassTrain - 0.1) + 1)  # NumPerClassTrain - skad to ma byc potem brane?
+#         class2 = int(np.math.floor(index2[2] / NumPerClassTrain - 0.1) + 1)
+#         class3 = int(np.math.floor(index2[3] / NumPerClassTrain - 0.1) + 1)
+#
+#         if (class1 != class2) & (class2 != class3):
+#             result = class1
+#         else:
+#             if class1 == class2:
+#                 result = class1
+#             if class2 == class3:
+#                 result = class2
+#         # end
+#
+#         div = div + 1
+#
+#         print "Result: " + str(result) + " Actual: " + str(i)
+#         if result == i:
+#             accu = accu + 1
+# # print accu
+# # print div
+# accuracyRate = float(accu)/float(div)
+#
 # print "Accuracy: " + str(accuracyRate)
