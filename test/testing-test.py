418cb0d1e3e8913ec2946d05b4a3fd98a8dbe968
Piotr Majorczyk <smajorc@gmail.comgit config --global user.email smajorc@gmail.comgit config --global user.email smajorc@gmail.com>
2017-08-24 17:36:33 +0200

    testing-test working fine

--- test/testing-test.py
+++ test/testing-test.py
@@ -195,74 +195,78 @@ matlab1.workspace.close()
 #
 # Testing
 
-# div = 0
-#
-# for i in range(1, NumPerson + 1):  # 1
-#     for j in range(NumPerClassTrain + 1, NumPerClass + 1):  # 1(1, NumPerClassTest + 1):
-#         pathname = filepath + "\\s" + str(i) + "\\" + str(j) + ".pgm"
-#
-#         img2 = cv2.imread(pathname, 0)
-#         img2 = ((img2 - np.mean(img2)) + 128) / np.std(img2) * 20
-#         iiCell = commons.block(numx, numy, img2)
-#         lbpII = []
-#         lbpII = np.asarray(lbpII)
-#
-#         for k in range(numx):
-#             for m in range(numy):
-#                 iiCellBlock = iiCell[k, m]  # k,m
-#                 blockLBPII = wlcgpFile.wlcgp(iiCellBlock)
-#                 blockLBPII = np.transpose(blockLBPII)
-#                 if (m == 0) & (k == 0):
-#                     lbpII = blockLBPII
-#                 else:
-#                     lbpII = np.concatenate((lbpII, blockLBPII))  # LBP_I=[LBP_I,Block_LBP_I]; moze byc concatenate
-#                     # end
-#         # end
-#         lbpII = lbpII.reshape(1, lbpII.shape[0])
-#         tcoor = lbpII.dot(base)  # lbpII * base// base trzeba tu liczyc!!! //rozmiar ok
-#
+div = 0
+
+for i in range(1, NumPerson + 1):  # 1
+    for j in range(NumPerClassTrain + 1, NumPerClass + 1):  # 1(1, NumPerClassTest + 1):
+        pathname = filepath + "\\s" + str(i) + "\\" + str(j) + ".pgm"
+
+        img2 = cv2.imread(pathname, 0)
+        img2 = ((img2 - np.mean(img2)) + 128) / np.std(img2) * 20
+        iiCell = commons.block(numx, numy, img2)
+        lbpII = []
+        lbpII = np.asarray(lbpII)
+
+        for k in range(numx):
+            for m in range(numy):
+                iiCellBlock = iiCell[k, m]  # k,m
+                blockLBPII = wlcgpFile.wlcgp(iiCellBlock)
+                blockLBPII = np.transpose(blockLBPII)
+                if (m == 0) & (k == 0):
+                    lbpII = blockLBPII
+                else:
+                    lbpII = np.concatenate((lbpII, blockLBPII))  # LBP_I=[LBP_I,Block_LBP_I]; moze byc concatenate
+                    # end
+        # end
+
+        lbpII = lbpII.reshape(1, lbpII.shape[0])
+        # print lbpII[:,2552] #ok
+        tcoor = lbpII.dot(base)  # lbpII * base// base trzeba tu liczyc!!! //rozmiar ok
+
 #         # print np.transpose(E).shape #ok
 #         # print np.transpose(tcoor).shape #ok
 #
-#         tcoor = np.transpose(E).dot(np.transpose(tcoor))  # E trzeba bedzie tu liczyc? ale z czego?
-#
+        tcoor = np.transpose(E).dot(np.transpose(tcoor))  # E trzeba bedzie tu liczyc? ale z czego?
+        # print tcoor
 #         # print tcoor.shape #ok
-#         # k = 1
-#         # mdist = []
-#         # while k <= nTrain:  # co z tym nTrain???
-#         #     k += 1
-#         mdist = [None] * nTrain
-#         for k in range(nTrain):
-#             mdist[k] = np.linalg.norm(tcoor - P[:, k]) #P trzeba wyciagac
-#         # end
-#
-#         ####################################
-#
+        # k = 1
+        # mdist = []
+        # while k <= nTrain:  # co z tym nTrain???
+        #     k += 1
+        mdist = [None] * nTrain
+        for k in range(nTrain):
+            mdist[k] = np.linalg.norm(tcoor - P[:, k]) #P trzeba wyciagac
+        # end
+        # print mdist
+        #do tej pory OK
 #         # 3 NN algorithm
 #         # dist, index2 = np.sort(mdist)
-#         index2 = np.argsort(mdist)
-#         dist = mdist.sort
+        index2 = np.argsort(mdist)
+        dist = np.sort(mdist)
+        # print index2 - jest w porzadku tylko sa diw trojki takich samych wartosci wiec moga wychodzic inne indexy
 #
-#         class1 = int(np.math.floor(index2[1] / NumPerClassTrain - 0.1) + 1)  # NumPerClassTrain - skad to ma byc potem brane?
-#         class2 = int(np.math.floor(index2[2] / NumPerClassTrain - 0.1) + 1)
-#         class3 = int(np.math.floor(index2[3] / NumPerClassTrain - 0.1) + 1)
-#
-#         if (class1 != class2) & (class2 != class3):
-#             result = class1
-#         else:
-#             if class1 == class2:
-#                 result = class1
-#             if class2 == class3:
-#                 result = class2
-#         # end
-#
-#         div = div + 1
-#
-#         print "Result: " + str(result) + " Actual: " + str(i)
-#         if result == i:
-#             accu = accu + 1
-# # print accu
-# # print div
-# accuracyRate = float(accu)/float(div)
-#
-# print "Accuracy: " + str(accuracyRate)
+        class1 = int(np.math.floor(index2[1] / NumPerClassTrain - 0.1) + 2)  # NumPerClassTrain - skad to ma byc potem brane?
+        class2 = int(np.math.floor(index2[2] / NumPerClassTrain - 0.1) + 2)
+        class3 = int(np.math.floor(index2[3] / NumPerClassTrain - 0.1) + 2)
+
+        # print class1
+
+        if (class1 != class2) & (class2 != class3):
+            result = class1
+        else:
+            if class1 == class2:
+                result = class1
+            if class2 == class3:
+                result = class2
+        # end
+
+        div = div + 1
+
+        print "Result: " + str(result) + " Actual: " + str(i)
+        if result == i:
+            accu = accu + 1
+# print accu
+# print div
+accuracyRate = float(accu)/float(div)
+
+print "Accuracy: " + str(accuracyRate)
